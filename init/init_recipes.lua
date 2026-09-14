599b1ad8cfecfb72cfbfe8310fbe92cd6d088941
xixiangkun <xixiangkun@gmail.com>
2026-09-14 09:46:45 +0800

    edit transformation cube craft

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -51,7 +51,7 @@ local CUBE_RECIPES =
 	{
 		name = "ms_messagebottle",
 		product = "messagebottle",
-		ingredient = "messagebottleempty",
+		ingredient = { "messagebottleempty", "papyrus" },
 		cost = 0.05,
 	},
 	{
