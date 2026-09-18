d1e9eafb670e1016636172fd18939cd2ab5d10cc
luigi.m.mario <luigi.m.mario@gmail.com>
2026-09-18 23:16:22 +0200

    Fixed bat filters

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -622,7 +622,7 @@ AddRecipe2("mountain_aluminum_axe", { Ingredient("twigs", 2), Ingredient("ms_alu
 AddRecipe2("mountain_super_gildedaxe", { Ingredient("goldenaxe", 1), Ingredient("ms_gold_detail", 1) }, TECH.SCIENCE_TWO, nil, {"TOOLS"})
 AddRecipe2("mountain_super_gildedshovel", { Ingredient("goldenshovel", 1), Ingredient("ms_gold_detail", 1) }, TECH.SCIENCE_TWO, nil, {"TOOLS"})
 -- Weapons
-AddRecipe2("mountain_copper_bat",  { Ingredient("ms_copper_detail", 2) }, TECH.SCIENCE_TWO, { image = "mountain_copper_bat_1.tex"}, nil, {"WEAPONS"})
+AddRecipe2("mountain_copper_bat",  { Ingredient("ms_copper_detail", 2) }, TECH.SCIENCE_TWO, { image = "mountain_copper_bat_1.tex"}, {"WEAPONS"})
 AddRecipe2("mountain_aluminum_dagger", { Ingredient("twigs", 2), Ingredient("ms_alu_detail", 1) }, TECH.SCIENCE_TWO, nil, {"WEAPONS"})
 AddRecipe2("mountain_yoth_lance", { Ingredient("twigs", 4), Ingredient("ms_bronze_detail", 2) }, TECH.SCIENCE_TWO, nil, {"WEAPONS"})
 AddRecipe2("mountain_wathgrithr_shield", { Ingredient("ms_bronze_detail", 2), Ingredient("pigskin", 1)}, TECH.SCIENCE_TWO, nil, {"WEAPONS", "ARMOUR"})
